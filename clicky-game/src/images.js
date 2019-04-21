c9504bd11c42b8546b965a6f14a34ddcfdc644dd
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-04-20 19:39:00 -0700

    created for loop for pushing images

--- clicky-game/src/images.js
+++ clicky-game/src/images.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import images from '../sanrio_characters'
+import images from './sanrio_characters/character_index';
 
 class Images extends React.Component {
-    
+
+    render() {
+
+        const imageList = [];
+        for(let i = 1; i <= 12; i++) {
+            imageList.push((
+                <div>
+                    <img src={images["img" + i]} alt="character" />
+                </div>
+            ))
+        }
+
+        return (
+            <div>
+                {imageList}
+            </div>
+        )
+    }
 }
 
 export default Images;
