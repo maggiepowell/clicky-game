7c2fe1256330793c1b164e3288a41e07da801e62
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-04-22 15:37:04 -0500

    added css styling

--- clicky-game/src/scorebar.js
+++ clicky-game/src/scorebar.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Scorebar = (props) => {
 
         return (
-            <div>
-            Score: {props.score}
+            <div className="scorebar">
+            <div id="title">Sanrio Memory Game!</div>
+            <div id="subtitle">Click on an image to earn points. Don't click on the same image twice!</div>
+            <div id="score">Score: {props.score}</div>
             </div>
         )
     }
